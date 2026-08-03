2b15ff0461a62bf4e63e4e0b2ff8a08de9d949c3
Kovawx <kovawx@gmail.com>
2026-08-03 14:58:40 +0800

    feat(settings): compose mixed inbound settings

---
+++ src/lib/components/settings/GeneralSettingsPanel.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import AppConfigPanel from '$lib/components/settings/AppConfigPanel.svelte';
+  import LocalProxyEndpointPanel from '$lib/components/settings/LocalProxyEndpointPanel.svelte';
+</script>
+
+<LocalProxyEndpointPanel />
+<AppConfigPanel />
