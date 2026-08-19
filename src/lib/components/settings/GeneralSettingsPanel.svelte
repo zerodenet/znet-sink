16c8d71c1c0af6397cf69c092d727fd6eaf09b08
Kovawx <kovawx@gmail.com>
2026-08-19 12:03:47 +0800

    feat(settings): show traffic ball preference

--- src/lib/components/settings/GeneralSettingsPanel.svelte
+++ src/lib/components/settings/GeneralSettingsPanel.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import AppConfigPanel from '$lib/components/settings/AppConfigPanel.svelte';
   import LocalProxyEndpointPanel from '$lib/components/settings/LocalProxyEndpointPanel.svelte';
+  import TrafficBallSettingsPanel from '$lib/components/settings/TrafficBallSettingsPanel.svelte';
   import UrlTestSettingsPanel from '$lib/components/settings/UrlTestSettingsPanel.svelte';
 </script>
 
+<TrafficBallSettingsPanel />
 <LocalProxyEndpointPanel />
 <UrlTestSettingsPanel />
 <AppConfigPanel />
