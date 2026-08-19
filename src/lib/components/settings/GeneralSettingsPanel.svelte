f1f0e2ba561967e7083e438afbdc2f5adc578a14
Kovawx <kovawx@gmail.com>
2026-08-19 13:09:58 +0800

    refactor(settings): keep general settings application-focused

--- src/lib/components/settings/GeneralSettingsPanel.svelte
+++ src/lib/components/settings/GeneralSettingsPanel.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
   import AppConfigPanel from '$lib/components/settings/AppConfigPanel.svelte';
-  import LocalProxyEndpointPanel from '$lib/components/settings/LocalProxyEndpointPanel.svelte';
-  import TrafficBallSettingsPanel from '$lib/components/settings/TrafficBallSettingsPanel.svelte';
-  import UrlTestSettingsPanel from '$lib/components/settings/UrlTestSettingsPanel.svelte';
 </script>
 
-<TrafficBallSettingsPanel />
-<LocalProxyEndpointPanel />
-<UrlTestSettingsPanel />
-<AppConfigPanel />
+<AppConfigPanel scope="general" />
